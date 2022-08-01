e8064d9c6febba332ea9ab26c829f2b48735dba7
virusofgod <virusofgod@yandex.ru>
2022-08-01 12:53:22 +0400

    lesson 17 example 2 finished class point

--- src/ru/virusofgod/Point.java
+++ src/ru/virusofgod/Point.java
@@ -1,4 +1,37 @@
 package ru.virusofgod;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Point {
+
+    double x, y;
+
+    void move(double dx, double dy){
+    x+=dx;
+    y+=dy;
+    }
+
+void input(){
+    Scanner in = new Scanner(System.in);
+    in.useLocale(Locale.US);
+    System.out.print("Enter x: ");
+    double x = in.nextDouble();
+    System.out.print("Enter y: ");
+    double y = in.nextDouble();
+    move(x, y);
+
+    }
+
+    double getDistance(Point a){
+    return  Math.sqrt(Math.pow(x-a.x, 2) + Math.pow(y-a.y, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
