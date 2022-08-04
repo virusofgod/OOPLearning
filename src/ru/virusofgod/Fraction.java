73bb5ffb1f80d570c760df71102ba50a27a64754
virusofgod <virusofgod@yandex.ru>
2022-08-04 19:51:42 +0400

    lesson 17 example 6 class Fraction  &  example constructors finished

--- src/ru/virusofgod/Fraction.java
+++ src/ru/virusofgod/Fraction.java
@@ -2,4 +2,22 @@ package ru.virusofgod;
 
 public class Fraction {
     int numerator;
+    int denumerator = 1;
+
+   public Fraction(int numerator, int denumerator){
+          if(denumerator==0){
+              System.out.println("denumerator can't be zero");
+              return;
+          }
+          this.numerator = numerator;
+          this.denumerator = denumerator;
+  }
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerator=" + numerator +
+                ", denumerator=" + denumerator +
+                '}';
+    }
 }
--- src/ru/virusofgod/constructorstest/main.java
+++ src/ru/virusofgod/constructorstest/main.java
@@ -1,19 +1,26 @@
 package ru.virusofgod.constructorstest;
 
+import ru.virusofgod.Fraction;
+
 public class main {
 
       public static void main(String[] args) {
-          Person person = new Person("Сергей", 28);
-          System.out.println( person.getName() + " id " + person.getId() ); //Реализация присвоения id каждому объекту
-          Person person2 = new Person("Михаил", 10);      //данного класса.
-          System.out.println( person2.getName() + " id " + person2.getId() );
-          Person person3 = new Person("Пётр", 8);
-          System.out.println( person3.getName() + " id " + person3.getId() );
-          Person person4 = new Person("Олег", 3);
-          System.out.println( person4.getName() + " id " + person4.getId() );
-          System.out.println( Person.getCounter() - 1);
 
+       //lesson 17 example 5 class Person
+        /*  {
+              Person person = new Person("Сергей", 28);
+              System.out.println(person.getName() + " id " + person.getId()); //Реализация присвоения id каждому объекту
+              Person person2 = new Person("Михаил", 10);      //данного класса.
+              System.out.println(person2.getName() + " id " + person2.getId());
+              Person person3 = new Person("Пётр", 8);
+              System.out.println(person3.getName() + " id " + person3.getId());
+              Person person4 = new Person("Олег", 3);
+              System.out.println(person4.getName() + " id " + person4.getId());
+              System.out.println(Person.getCounter() - 1);
+          }*/
 
+        Fraction fraction = new Fraction(0, 0);
+          System.out.println(fraction);
 
       }
 
