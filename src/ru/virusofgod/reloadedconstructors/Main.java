85232f456bd4e974d1f215dec20201d7d101e648
Gulyaev Alexander <virusofgod@yandex.ru>
2022-08-17 18:28:58 +0400

    lesson 17 example 7 forgot add class main in repo

--- src/ru/virusofgod/reloadedconstructors/Main.java
+++ src/ru/virusofgod/reloadedconstructors/Main.java
@@ -2,6 +2,9 @@ package ru.virusofgod.reloadedconstructors;
 
 public class Main {
     public static void main(String[] args) {
-        
+Person oleg = new Person("Konin");
+        System.out.println(oleg);
+
+
     }
 }
